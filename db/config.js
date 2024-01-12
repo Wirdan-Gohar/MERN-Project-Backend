561a76a91ea20bccd61c2c4ff4baa33701a28b55
Wirdan Gohar <wirdan@99technologies.co>
2024-01-12 19:06:56 +0500

    try

--- db/config.js
+++ db/config.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
-);
-const db = mongoose.connection;
+try {
+	mongoose.connect(
+		'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
+	);
+	const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-	console.log('Connected to MongoDB');
-});
+	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+	db.once('open', () => {
+		console.log('Connected to MongoDB');
+	});
+} catch (error) {
+	console.log('error', error);
+}
