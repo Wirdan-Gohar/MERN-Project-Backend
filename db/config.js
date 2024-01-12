eb5ca64e417777a6ee970629565d330493b67a51
Wirdan Gohar <wirdan@99technologies.co>
2024-01-12 18:16:40 +0500

    ss

--- db/config.js
+++ db/config.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-	// 'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
-	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/?retryWrites=true&w=majority'
+	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
 );
-// mongoose.connect(
-// 	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
-// );
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
