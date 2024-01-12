54b35c44d67dd48b530518f0fa709af631191366
Wirdan Gohar <wirdan@99technologies.co>
2024-01-12 18:45:35 +0500

    chal na jani

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const port = 5000;
+
 app.use(express.json());
 app.get('/', (req, res) => {
 	res.send('Backend is running Fine');
+});
+// Create Products API
+
+// app.get('/register', (req, res) => {
+// 	res.send(req.body);
+// });
+
+app.listen(port, () => {
+	console.log(`https://localhost:${port}`);
 });
