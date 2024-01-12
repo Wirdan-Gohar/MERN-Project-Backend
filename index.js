587f4efff0155c51f09ed19149cc1fdd4235fe1b
Wirdan Gohar <wirdan@99technologies.co>
2024-01-12 18:50:23 +0500

    dsf

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const mongoose = require('mongoose');
 app.use(express.json());
 app.get('/', (req, res) => {
 	res.send('Backend is running Fine');
 });
-// Create Products API
-
-// app.get('/register', (req, res) => {
-// 	res.send(req.body);
-// });
 
 app.listen(port, () => {
 	console.log(`https://localhost:${port}`);
