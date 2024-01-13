fa07daf488f7f4ecf8ad5de99bc0f3293bae4280
Wirdan Gohar <wirdan@99technologies.co>
2024-01-13 19:28:44 +0500

    update files Successfully

--- application_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# cd /var/www/Mern/back-end
-# sudo npm install
-# chal ja bhosdika
--- before_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# sudo find /var/www/Mern/back-end -mindepth 1 -not -name 'node_modules' -not -path '/var/www/html/node_modules*' -exec rm -rf {} +
