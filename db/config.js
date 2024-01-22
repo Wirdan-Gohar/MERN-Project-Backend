e6ed39e207a2d6fcb2a2718229858ee7925ae8e1
Wirdan Gohar <wirdan@99technologies.co>
2024-01-22 12:41:15 +0500

    mongo db uri added

--- db/config.js
+++ db/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
+	// 'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
+	'mongodb+srv://vercel-admin-user-65ae165e3afb9a5b4b5ad8f1:txFBHU0IifxIxL8H@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
 );
 const db = mongoose.connection;
 
