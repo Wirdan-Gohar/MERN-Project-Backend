6c91ab7852027d0bdbe52cdca256072567a85898
Wirdan Gohar <wirdan@99technologies.co>
2024-01-22 12:39:21 +0500

    mongo db connect

--- db/config.js
+++ db/config.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose');
 
-try {
-	mongoose.connect(
-		// 'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority',
-		'mongodb+srv://vercel-admin-user-65ae165e3afb9a5b4b5ad8f1:txFBHU0IifxIxL8H@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority',
+mongoose.connect(
+	'mongodb+srv://wirdan:pakistan@cluster0.3a1op15.mongodb.net/e-commerce?retryWrites=true&w=majority'
+);
+const db = mongoose.connection;
 
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			ssl: true, // Enable SSL
-			sslValidate: true, // Validate SSL certificates
-		}
-	);
-
-	const db = mongoose.connection;
-
-	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-	db.once('open', () => {
-		console.log('Connected to MongoDB');
-	});
-} catch (error) {
-	console.error('Error connecting to MongoDB:', error);
-}
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+	console.log('Connected to MongoDB');
+});
